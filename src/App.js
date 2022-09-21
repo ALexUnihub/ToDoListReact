de90d93b8f16b956028661284f65cc99ccc9210c
Alexander Chernenko <a.chernenko@bellerage.com>
2022-09-21 11:09:37 +0300

    list

--- src/App.js
+++ src/App.js
@@ -4,90 +4,91 @@ import {Title} from './elements/title';
 import {ItemsList} from './elements/itemList';
 // import {AddButton, DeleteButton} from './elements/button';
 import Button from '@mui/material/Button';
-import {DataGrid, GridValueGetterParams} from '@mui/x-data-grid';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import Paper from '@mui/material/Paper';
+import { Table } from '@mui/material';
+import TableRow from '@mui/material/TableRow';
+import DeleteOutlined from '@mui/icons-material/DeleteOutlined';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+
 
 function App() {
-  let columns = [
+  let newElems = [
     {
-      id: 0,
       ID: 0,
-      firstName: 'Title_1',
-      lastName: 'Text_1',
+      Title: 'Title_1',
+      Text: 'Text_1',
       IsDone: true,
     },
     {
-      id: 1,
       ID: 1,
-      firstName: 'Title_2',
-      lastName: 'Text_2',
+      Title: 'Title_2',
+      Text: 'Text_2',
       IsDone: false,
     },
     {
-      id: 2,
       ID: 2,
-      firstName: 'Title_3',
-      lastName: 'Text_3',
+      Title: 'Title_3',
+      Text: 'Text_3Text_3Text_3Text_3Text_3Text_3 Text_3',
       IsDone: true,
     },
   ]
-  let rows = [
-    { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-  ]
+
+  const reactElems = createItems(newElems);
+  const title = "To Do List";
 
   return (
     <div className='container'>
-      {/* <Title />
-      <ItemsList /> */}
-      <DataGrid
-        rows={columns}
-        columns={rows}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-      />
-      <div className='container__btns'>
-        {/* <AddButton name="Add new one"/> */}
-        <Button variant='contained'>Add new one</Button>
-      </div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+          <TableBody>
+            <TableRow>
+              <TableCell align='center'>
+                {title}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+          <TableBody>
+            {reactElems}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Button variant='contained' sx={{
+        width: 150,
+        right: 0,
+        position: 'absolute',
+      }}>Add new one</Button>
     </div>
-  )
+  );
 }
 
 export {App};
+
+function createItems(elems) {
+  const arr = elems.map((row, idx) => {
+    return (<TableRow key={row.Text}>
+      <TableCell style={{ width: 50 }} component="th" scope="row">
+        {idx + 1}
+      </TableCell>
+      <TableCell style={{ minWidth: 160 }} align="left">
+        {row.Title}
+      </TableCell>
+      <TableCell align="left">
+        {row.Text}
+      </TableCell>
+      <TableCell style={{ width: 52 }}>
+        <Button style={{ width: 44 }}>Edit</Button>
+      </TableCell>
+      <TableCell style={{ width: 52 }}>
+        <DeleteOutlined sx={{ fontSize: 24, width: 44, color: '#1976d2' }} className='testName'></DeleteOutlined>
+      </TableCell>
+    </TableRow>);
+  });
+
+  console.log(arr);
+  return arr;
+}
